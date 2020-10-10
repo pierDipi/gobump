044dfd65861c541d520cfeacda9708d3cd1fe766
Pierangelo Di Pilato <pierangelodipilato@gmail.com>
2020-10-10 13:23:33 +0200

    Add -exclude-regex flag (#2)
    Signed-off-by: Pierangelo Di Pilato <pierangelodipilato@gmail.com>

--- cmd/gobump/main.go
+++ cmd/gobump/main.go
@@ -4,9 +4,9 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"regexp"
 
 	. "github.com/pierdipi/gobump"
-	skip "github.com/pierdipi/gobump"
 )
 
 func main() {
@@ -17,12 +17,26 @@ func main() {
 
 func run() error {
 
+	skips := []SkipFile{
+		NonGoMod,
+		Vendor,
+	}
+
 	target := flag.String("target", "", "Go target version")
+	exclude := flag.String("exclude-regex", "", "Exclude regex")
 	flag.Parse()
 
 	if target == nil || *target == "" {
 		return fmt.Errorf("invalid target")
 	}
+	if exclude != nil && *exclude != "" {
+		excludeRegex, err := regexp.Compile(*exclude)
+		if err != nil {
+			return fmt.Errorf("invalid exclude regex: %w", err)
+		}
 
-	return ApplyTarget(*target, ".", ReadWriter, skip.NonGoMod, skip.Vendor)
+		skips = append(skips, Exclude(excludeRegex))
+	}
+
+	return ApplyTarget(*target, ".", ReadWriter, skips...)
 }
